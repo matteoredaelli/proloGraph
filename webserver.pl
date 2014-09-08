734801cc88f6c264fae8e927bab1b90f8b35b716
matteoredaelli <matteo.redaelli@gmail.com>
2014-09-08 16:17:07 +0200

    adding json response instead of plain text

--- webserver.pl
+++ webserver.pl
@@ -5,6 +5,7 @@
 :- use_module(library(uri)). 
 
 :- use_module(library(http/json)).
+:- use_module(library(http/http_json)).
 :- use_module(library(http/json_convert)).
 
 :- http_handler(root(hello_world), say_hi, []).
@@ -16,13 +17,11 @@
 server(Port) :-
     http_server(http_dispatch, [port(Port)]).
 
-edge_to_term([E1, Rel, E2], T) :-
+edge_to_term_json([E1, Rel, E2], json([from=E1A, to=E2A, rel=Rel])) :-
     term_to_atom(E1, E1A),
-    term_to_atom(E2, E2A),
-    atomic_list_concat([E1A, Rel, E2A], ' ', T).
+    term_to_atom(E2, E2A).
 
 show_vertex(Request) :-
-    format('Content-type: text/plain~n~n'),
     http_parameters(Request,
 		    [
 		     name(NameString,   [])
@@ -32,14 +31,16 @@ show_vertex(Request) :-
     %% previous vertexes
     %%
     prev_vertexes(Name, PrevEdges),
-    maplist(edge_to_term, PrevEdges, PE),
-    atomic_list_concat(PE, '~n', PEL),
-    format(PEL),
-    format('~n'),
+    maplist(edge_to_term_json, PrevEdges, PE),
+
     %% 
     %% next vertexes
     %%
     next_vertexes(Name, NextEdges),
-    maplist(edge_to_term, NextEdges, NE),
-    atomic_list_concat(NE, '~n', NEL),
-    format(NEL).
+    maplist(edge_to_term_json, NextEdges, NE),
+
+    prolog_to_json(json([prev=PE,
+			 next=NE]), Json),
+    reply_json(Json).
+
+
